2d0878ce9eb9a769ef37574dd47d4828c0a28e9c
“sreelatha” <sreelatha.vallabhaneni@gmail.com>
2025-07-31 20:40:58 +0530

    updated user credentials to auth service and controller

--- backend/api/src/auth/auth.service.ts
+++ backend/api/src/auth/auth.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
 import * as bcrypt from 'bcrypt';
-import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService,
+    private jwtService: JwtService
   ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByUsername(username);
     if (user && await bcrypt.compare(pass, user.password)) {
+    //if (user && user.password === pass) {
       const { password, ...result } = user;
       return result;
     }
@@ -23,6 +24,11 @@ export class AuthService {
     const payload = { username: user.username, sub: user.id, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
+      user: {
+        id: user.id,
+        username: user.username,
+        role: user.role,  // ⬅️ send the role here
+      }
     };
   }
 }
